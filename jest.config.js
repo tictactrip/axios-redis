8c4e73b804335a78358f41789b0fe09603f28e3c
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2020-01-15 15:35:59 +0100

    chore(jest.config.js): testRegex updated

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'json'],
-  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
 };
